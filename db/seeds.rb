3b9cb525e6b09cb6bde2299c98b57ae8b903155c
pharia-le <pharia.le@gmail.com>
2020-07-25 17:48:32 -0700

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+emails = [
+    'user_1@example.com',
+    'user_2@example.com',
+    'user_3@example.com',
+    'user_4@example.com',
+    'user_5@example.com'
+]
+
+usernames = [
+    'user_1',
+    'user_2',
+    'user_3',
+    'user_4',
+    'user_5'
+]
+
+names = [
+    'Name One',
+    'Name Two',
+    'Name Three',
+    'Name Four',
+    'Name Five'
+]
+
+habits = [
+    'Meditate',
+    'Exercise',
+    'Drink 6 Cups of Water',
+    'Stay Home'
+]
+
+dates = [
+    Date.today,
+    Date.yesterday
+]
+
+
+emails.each_with_index() { |email, i|
+    User.create(
+    email: email,
+    username: usernames[i],
+    name: names[i],
+    password: 'password'
+    )
+}
+
+User.all.each { |user|
+    habits.each_with_index() { |habit, i|
+        Habit.create(
+            name: habits[i],
+            user: user
+        )}
+}
+
+Habit.all.each { |habit|
+    dates.each_with_index() { |date, i|
+        Log.create(
+            date: dates[i],
+            habit: habit
+        )}
+}
