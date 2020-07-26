87ff9feab6a658a2015265dde2ed5d311d644380
pharia-le <pharia.le@gmail.com>
2020-07-25 17:48:15 -0700

    Generate scaffold for log, perform migrate & update relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :habits
+    has_many :logs, through: :habits
 
     validates :email, uniqueness: true, presence: true
     validates :username, uniqueness: true, presence: true
