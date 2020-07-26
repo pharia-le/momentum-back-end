5791a90ab6c51708056cb722ecfdd32215ef48cb
pharia-le <pharia.le@gmail.com>
2020-07-25 17:36:31 -0700

    Generate scaffold for habit, perform migrate & update relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    
+    has_many :habits
+
     validates :email, uniqueness: true, presence: true
     validates :username, uniqueness: true, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-
+  
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :habits
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
