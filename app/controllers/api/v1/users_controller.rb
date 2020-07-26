4f39b972d6cef619835d37c9df2daf4a2450c138
pharia-le <pharia.le@gmail.com>
2020-07-25 18:09:45 -0700

    Generate FastJsonapi serializer for user & update UsersController.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,19 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # # GET /users
-  # def index
-  #   @users = User.all
+  # GET /users
+  def index
+    @users = User.all
 
-  #   render json: @users
-  # end
+    json_users = UserSerializer.new(@users).serialized_json
+    render json: json_users
+  end
 
-  # # GET /users/1
-  # def show
-  #   render json: @user
-  # end
+  # GET /users/1
+  def show
+    json_user = UserSerializer.new(@user).serialized_json
+    render json: json_user
+  end
 
   # POST /users
   def create
