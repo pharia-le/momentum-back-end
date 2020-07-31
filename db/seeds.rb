446cec889b7ad27cf8dfcbf8980d6e91cc4f05e4
pharia-le <pharia.le@gmail.com>
2020-07-31 15:33:43 -0700

    Add 'faker' gem & update seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,29 +2,29 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-emails = [
-    'user_1@example.com',
-    'user_2@example.com',
-    'user_3@example.com',
-    'user_4@example.com',
-    'user_5@example.com'
-]
+# emails = [
+#     'user_1@example.com',
+#     'user_2@example.com',
+#     'user_3@example.com',
+#     'user_4@example.com',
+#     'user_5@example.com'
+# ]
 
-usernames = [
-    'user_1',
-    'user_2',
-    'user_3',
-    'user_4',
-    'user_5'
-]
+# usernames = [
+#     'user_1',
+#     'user_2',
+#     'user_3',
+#     'user_4',
+#     'user_5'
+# ]
 
-names = [
-    'Name One',
-    'Name Two',
-    'Name Three',
-    'Name Four',
-    'Name Five'
-]
+# names = [
+#     'Name One',
+#     'Name Two',
+#     'Name Three',
+#     'Name Four',
+#     'Name Five'
+# ]
 
 habits = [
     'Meditate',
@@ -33,33 +33,56 @@ habits = [
     'Stay Home'
 ]
 
-dates = [
-    Date.today,
-    Date.yesterday
-]
+# dates = [
+#     Date.today,
+#     Date.yesterday
+# ]
 
 
-emails.each_with_index() { |email, i|
-    User.create(
-    email: email,
-    username: usernames[i],
-    name: names[i],
-    password: 'password'
-    )
-}
+# emails.each_with_index() { |email, i|
+#     User.create(
+#     email: email,
+#     username: usernames[i],
+#     name: names[i],
+#     password: 'password'
+#     )
+# }
 
-User.all.each { |user|
-    habits.each_with_index() { |habit, i|
+# User.all.each { |user|
+#     habits.each_with_index() { |habit, i|
+#         Habit.create(
+#             name: habits[i],
+#             user: user
+#         )}
+# }
+
+# Habit.all.each { |habit|
+#     dates.each_with_index() { |date, i|
+#         Log.create(
+#             date: dates[i],
+#             habit: habit
+#         )}
+# }
+
+User.create(
+    name: "Pharia",
+    username: "phariale",
+    email: "pharia.le@gmail.com",
+    password: "password"
+)
+
+
+habits.each_with_index() { |habit, i|
         Habit.create(
             name: habits[i],
-            user: user
+            user: User.last
         )}
-}
 
-Habit.all.each { |habit|
-    dates.each_with_index() { |date, i|
+User.last.habits.each { |habit|
+    rand(50..150).times {
         Log.create(
-            date: dates[i],
+            date: Faker::Date.between(from: rand(150..211).days.ago, to: Date.today),
             habit: habit
-        )}
+        )
+    }
 }
