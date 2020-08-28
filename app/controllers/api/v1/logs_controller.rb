4e0c4b5768931c3eb1e3730dde2ba7e0e1229634
pharia-le <pharia.le@gmail.com>
2020-08-27 23:40:03 -0700

    Remove unused methods.

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,19 +1,6 @@
 class Api::V1::LogsController < ApplicationController
   before_action :set_log, only: [:show, :update, :destroy]
 
-  # GET /logs
-  def index
-    @logs = Log.all
-
-    render json: @logs
-  end
-
-  # GET /logs/1
-  def show
-    render json: @log
-  end
-
-  # POST /logs
   def create
     @log = Log.new(log_params)
     if @log.save
@@ -25,16 +12,6 @@ class Api::V1::LogsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /logs/1
-  def update
-    if @log.update(log_params)
-      render json: @log
-    else
-      render json: @log.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /logs/1
   def destroy
     if @log.destroy
       render json:  { data: "Log successfully destroyed" }, status: :ok
@@ -47,12 +24,10 @@ class Api::V1::LogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_log
       @log = Log.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def log_params
       params.require(:log).permit(:done, :date, :habit_id)
     end
